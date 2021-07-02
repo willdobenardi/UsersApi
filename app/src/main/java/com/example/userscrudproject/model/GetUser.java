91e94aeb8915a88391591cf238a52299d84b7bcb
willdobenardi <charkecilpb1@gmail.com>
2021-07-02 10:39:10 +0700

    changes

---
+++ app/src/main/java/com/example/userscrudproject/model/GetUser.java
@@ -0,0 +1,25 @@
+package com.example.userscrudproject.model;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class GetUser {
+    @SerializedName("data")
+    List<User> data;
+    @SerializedName("total")
+    String total;
+
+    public String getTotal() {
+        return total;
+    }
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
